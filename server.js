983850f41fc678add6103e477996e98a23cdd8c6
Zain Abidin <xan.abdn@gmail.com>
2022-06-18 01:07:49 +0100

    server is working

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
 
 
-
-
-
-
-app.listen(8800,()=>{
-    console.log("express is running")
+app.get('/',(req,res)=>{
+    res.send("hello world")
 })
+
+app.listen(8800, () => {
+  console.log("express is running");
+});
