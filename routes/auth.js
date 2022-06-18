bcc121f322bdeb96ba49f4894dfd20a73f58130d
Zain Abidin <xan.abdn@gmail.com>
2022-06-18 03:10:05 +0100

    register route working

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,23 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const User = require("../models/User");
+//const bcrypt = require("bcrypt");
 
-router.get("/",(req,res)=>{
-res.send("this is auth page")
-})
+//signup
+router.post("/register", async (req, res) => {
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.username,
+    
+  })
+  try{
+    const user = await newUser.save();
+    res.status(200).json(user);
+  }
+  catch(err){
+  console.log(err)
+  }
+});
 
-module.exports = router
+module.exports = router;
+
